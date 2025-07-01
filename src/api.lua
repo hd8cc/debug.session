650e44c5bd74f07748336d01b7c17b0f23ee9bc7
hd8 <seliwarev1@gmail.com>
2025-07-01 19:09:51 +0200

    Update api.lua

--- src/api.lua
+++ src/api.lua
@@ -1,1 +1,16 @@
-return(false)
+local v2 = {}
+if not game.IsLoaded() then
+  game.IsLoaded:Wait()
+end
+
+getSession = function()
+  local plr = game.Players.LocalPlayer
+  local pr = result
+  return pr
+end
+
+
+checkSession = function()
+  local ex = 'Unkonwn'
+  return getSession()
+end
